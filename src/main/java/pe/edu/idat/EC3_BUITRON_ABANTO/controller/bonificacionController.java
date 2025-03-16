097974d3fcc05e851eb1564d3668b6e82de04a16
Steven-Abanto <steven.abanto.xd@gmail.com>
2025-03-15 23:32:55 -0500

    tercer commit

--- src/main/java/pe/edu/idat/EC3_BUITRON_ABANTO/controller/bonificacionController.java
+++ src/main/java/pe/edu/idat/EC3_BUITRON_ABANTO/controller/bonificacionController.java
@@ -1,4 +1,41 @@
 package pe.edu.idat.EC3_BUITRON_ABANTO.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import pe.edu.idat.EC3_BUITRON_ABANTO.model.bonificacionModel;
+
+@Controller
 public class bonificacionController {
+    @GetMapping("/bonificacion")
+    public String formularioBonificacion(Model model){
+        model.addAttribute("bonificacionModel", new bonificacionModel());
+        model.addAttribute("visualizarboni", false);
+        return "bonificacion";
+    }
+    @PostMapping("/calcularbonif")
+    public String calcuarBonif(@ModelAttribute("bonificacionModel") bonificacionModel calcbonif, Model model){
+        Integer diasPago = calcbonif.getDiasPago(); int descuento = 0;
+        Double credito = calcbonif.getcredito();
+        String diagnostico = "Usted tiene descuento: ", estiloDiagnostico= "alert-primary";
+
+        if (diasPago < 7){
+            descuento = 10;
+            credito = credito * (1 - descuento/100);
+            diagnostico += descuento + "%";
+        } else if (diasPago>=7 & diasPago < 15){
+            descuento = 5;
+            credito = credito * (1 - descuento/100);
+            diagnostico += descuento + "%";
+        }else {
+            diagnostico = "Usted no tiene descuento.";
+            estiloDiagnostico = "alert-danger";
+        }
+        model.addAttribute("Resultado","Situación: " + diagnostico + " Su credito final es = S/" + String.format("%.2f",credito));
+        model.addAttribute("visualizarboni", true);
+        model.addAttribute("estilodiagnostico", estiloDiagnostico);
+        return "bonificacion";
+    }
 }
--- src/main/java/pe/edu/idat/EC3_BUITRON_ABANTO/model/bonificacionModel.java
+++ src/main/java/pe/edu/idat/EC3_BUITRON_ABANTO/model/bonificacionModel.java
@@ -2,6 +2,22 @@ package pe.edu.idat.EC3_BUITRON_ABANTO.model;
 
 public class bonificacionModel {
 
+    private Double credito;
+    private Integer diasPago;
 
+    public Double getcredito() {
+        return credito;
+    }
 
+    public void setcredito(Double credito) {
+        this.credito = credito;
+    }
+
+    public Integer getDiasPago() {
+        return diasPago;
+    }
+
+    public void setDiasPago(Integer diasPago) {
+        this.diasPago = diasPago;
+    }
 }
